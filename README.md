c8474aedf3f72a550251a99a1343bcffa44b8607
joel081112 <JFerguson@sbs.local>
2019-09-18 12:38:04 +0100

    blsh

--- README.md
+++
@@ -1,3 +0,0 @@
-# fortress-sefa
-
-My first respo
--- manage.py
+++ manage.py
@@ -5,11 +5,7 @@ import sys
 
 
 def main():
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebP1.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DEMOPROJECT.settings')
->>>>>>> 5aea44f6da8ae60b66f5b9fa4fd7146c0a02ed9f
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,11 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-<<<<<<< HEAD
         ) from exc
-=======
-        )(exc)
->>>>>>> 5aea44f6da8ae60b66f5b9fa4fd7146c0a02ed9f
     execute_from_command_line(sys.argv)
 
 
