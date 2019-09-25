0ede0529c055eea5a0870ebde5112df2bd421910
joel081112 <JFerguson@sbs.local>
2019-09-25 09:09:22 +0100

    Solved issue with burger(literally didnt need the animate code in the js file)

--- DEMOPROJECT/static/js/hamburgerAnim.js
+++ DEMOPROJECT/static/js/hamburgerAnim.js
@@ -1,15 +1,3 @@
-// Auto animate for demo
-
-var toggle = document.querySelector('.toggle input')
-var animate = setInterval(() => {
-    toggle.checked = !toggle.checked
-}, 3000)
-
-document.querySelector('body').addEventListener('click', () => {
-    clearInterval(animate);
-})
-
-
 function displayWindowSize() {
     var w = document.documentElement.clientWidth;
     var toggle = document.querySelector(".toggle input");
