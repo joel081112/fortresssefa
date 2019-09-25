ee76d0bd35ab6212ed1a1aa0388ead25bb24849a
joel081112 <JFerguson@sbs.local>
2019-09-25 14:00:00 +0100

    burger responsive to clicks, still working on screen resizing

--- DEMOPROJECT/static/js/hamburgerAnim.js
+++ DEMOPROJECT/static/js/hamburgerAnim.js
@@ -1,11 +1,24 @@
 function displayWindowSize() {
     var w = document.documentElement.clientWidth;
     var toggle = document.querySelector(".toggle input");
+    var menu = document.getElementById('menuPanel');
 
     if (w > 640) {
         toggle.checked = false;
+        menu.style.cssText += ';display:block !important;'
     }
+    //going from small to large displays menu (as wanted)
+    //going back to small menu will always menu
+    //need to hide it when going to small
 }
 
 window.addEventListener("resize", displayWindowSize);
+
+var menu = document.getElementById('menuPanel');
+var checkbox = document.getElementById('myCheck');
+checkbox.onclick = function () {
+    this.checked ?
+        menu.style.cssText += ';display:block !important;' : menu.style.cssText += ';display:none !important;'
+}
+
 
