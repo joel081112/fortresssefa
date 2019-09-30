21fbbb5d343e736470fcda6c7ef628dd06eebc44
joel081112 <JFerguson@sbs.local>
2019-09-30 11:30:51 +0100

    Added mew pages

--- DEMOAPP/models.py
+++ DEMOAPP/models.py
@@ -10,72 +10,76 @@ from wagtail.search import index
 
 
 class ContactUs(Page):
-    # decide whats editable in backend
-    # do this instead of text field
-    # you can bold different words in it
-    # works like micro word
-    intro = RichTextField(
-        blank=True,  # required field or not
-        verbose_name="Intro"  # called on wagtail site
-    )
-    security = RichTextField(
-        blank=True,
-        verbose_name="Security"
-    )
-    environment = RichTextField(
-        blank=True,
-        verbose_name="Environment"
-    )
-    fire = RichTextField(
-        blank=True,
-        verbose_name="Fire"
-    )
-    architectural = RichTextField(
-        blank=True,
-        verbose_name="Architectural"
-    )
-
     search_fields = Page.search_fields + [
-        index.SearchField(
-            'intro',
-        ),
-        index.SearchField(
-            'security',
-        ),
-        index.SearchField(
-            'environment',
-        ),
-        index.SearchField(
-            'fire',
-        ),
-        index.SearchField(
-            'architectural',
-        ),
 
     ]  # these are if adding a search to the website
 
     # content tab panels
     content_panels = Page.content_panels + [
-        FieldPanel(
-            'intro',
-            classname="full"
-        ),
-        FieldPanel(
-            'security',
-            classname="full"
-        ),
-        FieldPanel(
-            'environment',
-            classname="full"
-        ),
-        FieldPanel(
-            'fire',
-            classname="full"
-        ),
-        FieldPanel(
-            'architectural',
-            classname="full"
-        ),
+
+    ]
+
+    # what to call the panels on wagtail
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(Page.promote_panels, heading='SEO'),
+        ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
+        # classname settings adds the cog
+    ]
+
+    )
+
+
+class About(Page):
+    search_fields = Page.search_fields + [
+
+    ]  # these are if adding a search to the website
+
+    # content tab panels
+    content_panels = Page.content_panels + [
+
+    ]
+
+    # what to call the panels on wagtail
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(Page.promote_panels, heading='SEO'),
+        ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
+        # classname settings adds the cog
+    ]
+
+    )
+
+
+class Products(Page):
+    search_fields = Page.search_fields + [
+
+    ]  # these are if adding a search to the website
+
+    # content tab panels
+    content_panels = Page.content_panels + [
+
+    ]
+
+    # what to call the panels on wagtail
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(Page.promote_panels, heading='SEO'),
+        ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
+        # classname settings adds the cog
+    ]
+
+    )
+
+
+class ProTec(Page):
+    search_fields = Page.search_fields + [
+
+    ]  # these are if adding a search to the website
+
+    # content tab panels
+    content_panels = Page.content_panels + [
+
     ]
 
     # what to call the panels on wagtail
