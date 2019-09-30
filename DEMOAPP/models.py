dff36d498f4730354a2d5582b9d5a90d4cf9e653
joel081112 <JFerguson@sbs.local>
2019-09-30 10:33:34 +0100

    Linking pages

--- DEMOAPP/models.py
+++ DEMOAPP/models.py
@@ -9,6 +9,86 @@ from wagtail.core.models import Page
 from wagtail.search import index
 
 
+class ContactUs(Page):
+    # decide whats editable in backend
+    # do this instead of text field
+    # you can bold different words in it
+    # works like micro word
+    intro = RichTextField(
+        blank=True,  # required field or not
+        verbose_name="Intro"  # called on wagtail site
+    )
+    security = RichTextField(
+        blank=True,
+        verbose_name="Security"
+    )
+    environment = RichTextField(
+        blank=True,
+        verbose_name="Environment"
+    )
+    fire = RichTextField(
+        blank=True,
+        verbose_name="Fire"
+    )
+    architectural = RichTextField(
+        blank=True,
+        verbose_name="Architectural"
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField(
+            'intro',
+        ),
+        index.SearchField(
+            'security',
+        ),
+        index.SearchField(
+            'environment',
+        ),
+        index.SearchField(
+            'fire',
+        ),
+        index.SearchField(
+            'architectural',
+        ),
+
+    ]  # these are if adding a search to the website
+
+    # content tab panels
+    content_panels = Page.content_panels + [
+        FieldPanel(
+            'intro',
+            classname="full"
+        ),
+        FieldPanel(
+            'security',
+            classname="full"
+        ),
+        FieldPanel(
+            'environment',
+            classname="full"
+        ),
+        FieldPanel(
+            'fire',
+            classname="full"
+        ),
+        FieldPanel(
+            'architectural',
+            classname="full"
+        ),
+    ]
+
+    # what to call the panels on wagtail
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(Page.promote_panels, heading='SEO'),
+        ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
+        # classname settings adds the cog
+    ]
+
+    )
+
+
 class HomePage(Page):
     # decide whats editable in backend
     # do this instead of text field
@@ -52,7 +132,6 @@ class HomePage(Page):
             'architectural',
         ),
 
-
     ]  # these are if adding a search to the website
 
     # content tab panels
@@ -88,4 +167,3 @@ class HomePage(Page):
     ]
 
     )
-
