b3785bb3483e5492c8f67899de50de94a2f1087e
joel081112 <JFerguson@sbs.local>
2019-10-07 13:41:12 +0100

    Got javascript working, with slight issue of text disappearing on div hitting top of screen not text touching div

---
+++ DEMOPROJECT/static/js/homepage.js
@@ -0,0 +1,22 @@
+$(function () {
+    var text = $(".text");
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 200) {
+            text.removeClass("hidden");
+        } else {
+            text.addClass("hidden");
+        }
+        if (scroll > $('.homeIm2').offset().top) { // when the div with homeIm2 class scrolls into view
+            text.hide();
+        }
+        if (scroll < $('.homeIm2').offset().top) { // when the div with homeIm2 class scrolls into view
+            text.show();
+        }
+
+    });
+});
+
+
+
