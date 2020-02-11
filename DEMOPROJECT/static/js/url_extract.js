95a90c07e9b416d7ad3413c7e45eae4ee761245f
joel081112@icloud.com <JFerguson@sbs.local>
2020-02-11 12:30:55 +0000

    Fixed javascript queries

--- DEMOPROJECT/static/js/url_extract.js
+++ DEMOPROJECT/static/js/url_extract.js
@@ -1,12 +1,20 @@
+$(function () {
+    document.getElementById("demo").innerHTML =
+        "The full URL of this page is:<br>" + window.location.href;
+});
 
+$(function () {
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
 
-document.getElementById("demo").innerHTML =
-"The full URL of this page is:<br>" + window.location.href;
+var password = url.searchParams.get("password");
+document.getElementById("demo3").innerHTML = password;
+});
 
+$(function () {
 var url_string = window.location.href; //window.location.href
 var url = new URL(url_string);
 var email = url.searchParams.get("email");
 document.getElementById("demo2").innerHTML = email;
+});
 
-var password = url.searchParams.get("password");
-document.getElementById("demo3").innerHTML = password;
