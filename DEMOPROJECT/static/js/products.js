b851bab96a120a93b55eaa5ac473623b4672c50f
joel081112@icloud.com <JFerguson@sbs.local>
2020-06-24 11:37:13 +0100

    About to play with thumbs gallery

--- DEMOPROJECT/static/js/products.js
+++ DEMOPROJECT/static/js/products.js
@@ -1,11 +1,35 @@
 $(".carousel-indicators").each(function () {
-    if ($('ul > li').length > 4) {
+    if ($('ul > li').length > 9) {
         $('ul').hide();
     }
 
-    if ($('.modal01').css('display') == 'block') {
-        $('ul').hide();
-    }
 
 });
 
+function myFunction() {
+  document.getElementById("modal").src = $('.active').find('img').attr('src');
+}
+
+
+/*Test*/
+$('#multiViewCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
